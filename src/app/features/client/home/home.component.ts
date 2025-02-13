0c4748a4c183ff407b49e128a0efce56736887fb
Trien Tran <trienilu3000@gmail.com>
2025-02-13 22:01:49 +0700

    Coding: Update Home Page

--- src/app/features/client/home/home.component.ts
+++ src/app/features/client/home/home.component.ts
@@ -2,11 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ProductCardComponent } from 'src/app/shared/common/product-card/product-card.component';
 import { CarouselComponent } from '../../../shared/common/carousel/carousel.component';
+import { BannerComponent } from './banner/banner.component';
+import { ProductSliderComponent } from './product-slider/product-slider.component';
 
 @Component({
   selector: 'aum-home',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent, CarouselComponent],
+  imports: [
+    CommonModule,
+    ProductCardComponent,
+    CarouselComponent,
+    BannerComponent,
+    ProductSliderComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
